5f63583db7b145cbe7020be431b04492d09c05f0
Athos Benther <athos@benther.com>
2024-09-21 12:23:44 -0300

    fiat Trait

--- src/Taits/ApiResponses.php
+++ src/Taits/ApiResponses.php
@@ -1,3 +1,81 @@
 <?php
 
 namespace AthosBenther\LaravelApiResponses\Traits;
+
+use AthosBenther\LaravelApiResponses\ApiResponse;
+use Exception;
+use Illuminate\Contracts\Support\Arrayable;
+use Symfony\Component\HttpFoundation\Response;
+
+trait ApiResponses
+{
+
+    /**
+     * Generates a success response.
+     *
+     * @param int $statusCode The HTTP status code.
+     * @param string|null $message The response message. If null, default message for the given status code will be used.
+     * @param array|null $data Additional data to include in the response.
+     * @param int $jsonEncodingFlags Response JSON Encoding flags
+     * @return Response The HTTP response.
+     */
+    public function Response(
+        string $message = null,
+        array|Arrayable $data = null,
+        int $statusCode = Response::HTTP_OK,
+        int $jsonEncodingFlags = 0
+    ): Response {
+        return new ApiResponse(
+            $message,
+            $data,
+            $statusCode,
+            $jsonEncodingFlags
+        );
+    }
+
+    /**
+     * Generates a success response.
+     *
+     * @param Exception $exception The exception.
+     * @param int $statusCode The HTTP status code.
+     * @param bool $trace Indicates if the response should contain the stack trace or not.
+     * @param int $jsonEncodingFlags Response JSON Encoding flags
+     * 
+     * @return Response The HTTP response.
+     */
+    public function ExceptionResponse(
+        Exception $exception,
+        int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR,
+        bool $trace = false,
+        int $jsonEncodingFlags = 0
+    ): Response {
+        $data =
+            [
+                'code' => $exception->getCode(),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'code' => $exception->getCode(),
+            ];
+
+        if ($exception->getPrevious()) $data['previous'] = $exception->getPrevious()->getMessage();
+
+        if ($trace) $data['trace'] = $exception->getTrace();
+
+        return new ApiResponse(
+            $exception->getMessage(),
+            $data,
+            $statusCode,
+            $jsonEncodingFlags
+        );
+    }
+
+    /**
+     * Generates a response for a not implemented feature.
+     *
+     * @return Response The HTTP response with status code 501.
+     */
+    public function NotImplemented(): Response
+    {
+        return $this->Response(null, null, 501);
+    }
+}
