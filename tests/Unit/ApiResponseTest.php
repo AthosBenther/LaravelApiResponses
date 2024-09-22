4187c94070ffd6c4db6a94b0ebcff836a6f0b0b2
Athos Benther <athos@benther.com>
2024-09-22 00:27:37 -0300

    Adds Trait tests and finishes covering 100% of the code

--- tests/Unit/ApiResponseTest.php
+++ tests/Unit/ApiResponseTest.php
@@ -1,76 +1,98 @@
 <?php
 
 use AthosBenther\LaravelApiResponses\ApiResponse;
+use Illuminate\Contracts\Support\Arrayable;
 use Tests\Extras\ArrayableObject;
+use Tests\TestCase;
+
+
+beforeEach(function () {
+    $this->message = TestCase::randomString();
+    $this->status = TestCase::getRandomHttpStatus();
+    $this->data = TestCase::randomArray();
+
+    $this->apiResponse = new ApiResponse($this->message, $this->data, $this->status['code']);
+});
 
 describe('ApiResponse', function () {
-    $apiResponse = new ApiResponse();
-
-    $expected = [
-        "message" => "OK",
-        "status" => [
-            "code" => 200,
-            "message" => "OK"
-        ],
-        "meta" => []
-    ];
-
-    it('Constructs', function () use ($apiResponse, $expected) {
-        $content = $apiResponse->getContent();
-        $result = json_decode($content, true);
-        unset($result['meta']['timestamp']);
-
-        expect($result)->toBe($expected);
-    });
-
-    it('sets Message', function () use ($apiResponse) {
-        $random = substr(md5(mt_rand()), 0, 7);
-        $content = $apiResponse->message($random)->getContent();
+    it('Constructs', function () {
+        $content = $this->apiResponse->getContent();
         $result = json_decode($content, true);
 
-        expect($result['message'])->toBe($random);
+        expect($content)->toBeJson();
+
+        expect($result['message'])->toBe($this->message);
+        expect($result['status']['code'])->toBe($this->status['code']);
+        expect($result['status']['message'])->toBe($this->status['message']);
+
+
+        expect($result['data'])->toBe($this->data);
+        expect($result['meta']['timestamp'])->toBeString();
     });
 
-    it('sets Data as Array', function () use ($apiResponse) {
-        $data = [];
+    it('sets forces Json response', function () {
+        $randomMessage = TestCase::randomString();
+        $headers = $this->apiResponse->headers->all();
 
-        for ($i = 0; $i < mt_rand(3, 10); $i++) {
-            $data[] = [substr(md5(mt_rand()), 0, 7) => substr(md5(mt_rand()), 0, 7)];
-        }
+        expect($headers)->toMatchArray([
+            "content-type" => [
+                "application/json"
+            ]
+        ]);
+    });
 
-        $content = $apiResponse->data($data)->getContent();
+    it('sets Message', function () {
+        $randomMessage = TestCase::randomString();
+        $content = $this->apiResponse->message($randomMessage)->getContent();
+        $result = json_decode($content, true);
+
+        expect($result['message'])->toBe($randomMessage);
+    });
+
+    it('sets Data as Array', function () {
+        $data = TestCase::randomArray();
+
+        $content = $this->apiResponse->data($data)->getContent();
         $result = json_decode($content, true);
 
         expect($result['data'])->toBe($data);
     });
 
-    it('sets Data as Arrayable', function () use ($apiResponse) {
-        $data = [];
+    it('sets Data as Arrayable', function () {
+        $data = TestCase::randomArray();
 
-        for ($i = 0; $i < mt_rand(3, 10); $i++) {
-            $data[] = [substr(md5(mt_rand()), 0, 7) => substr(md5(mt_rand()), 0, 7)];
-        }
+        $arrayable = new class implements Arrayable {
+            public array $data;
+            public function toArray()
+            {
+                return $this->data;
+            }
+        };
 
-        $arrayable = new ArrayableObject($data);
+        $arrayable->data = $data;
 
-        $content = $apiResponse->data($arrayable)->getContent();
+        $content = $this->apiResponse->data($arrayable)->getContent();
         $result = json_decode($content, true);
 
         expect($result['data'])->toBe($data);
     });
 
-    it('sets Meta', function () use ($apiResponse) {
-        $data = [];
+    it('sets Meta', function () {
+        $data = TestCase::randomArray();
 
-        for ($i = 0; $i < mt_rand(3, 10); $i++) {
-            $data[] = [substr(md5(mt_rand()), 0, 7) => substr(md5(mt_rand()), 0, 7)];
-        }
-
-        $content = $apiResponse->meta($data)->getContent();
+        $content = $this->apiResponse->meta($data)->getContent();
         $result = json_decode($content, true);
-        unset($result['meta']['timestamp']);
 
+        expect($result['meta'])->toMatchArray($data);
+    });
 
-        expect($result['meta'])->toBe($data);
+    it('sets Header', function () {
+        $data = TestCase::randomArray(3, 15, 3, 'key-', "");
+        $expectedData = array_map(fn($v) => [$v], $data);
+        $this->apiResponse->headers($data);
+
+        $headers = $this->apiResponse->headers->all();
+
+        expect($headers)->toMatchArray($expectedData);
     });
 });
---
+++ tests/Unit/ApiResponsesTraitTest.php
@@ -0,0 +1,93 @@
+<?php
+
+use AthosBenther\LaravelApiResponses\Traits\ApiResponses;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+describe('ApiResponse', function () {
+    $apiResponses = new class {
+        use ApiResponses;
+    };
+
+    it('can generate a basic Response', function () use ($apiResponses) {
+        $message = TestCase::randomString();
+        $status = TestCase::getRandomHttpStatus();
+        $data = TestCase::randomArray();
+
+        $content = $apiResponses->Response(
+            $message,
+            $data,
+            $status['code']
+        )->getContent();
+
+        $result = json_decode($content, true);
+
+        expect($content)->toBeJson();
+
+        expect($result['message'])->toBe($message);
+        expect($result['status']['code'])->toBe($status['code']);
+        expect($result['status']['message'])->toBe($status['message']);
+
+
+        expect($result['data'])->toBe($data);
+        expect($result['meta']['timestamp'])->toBeString();
+    });
+
+    it('can generate a Message Response', function () use ($apiResponses) {
+        $message = TestCase::randomString();
+        $status = TestCase::getRandomHttpStatus();
+
+        $content = $apiResponses->messageResponse($message, $status['code'])->getContent();
+        $result = json_decode($content, true);
+
+        expect($result['message'])->toBe($message);
+        expect($result['status']['code'])->toBe($status['code']);
+        expect($result['status']['message'])->toBe($status['message']);
+    });
+
+    it('can generate a Status Code Response', function () use ($apiResponses) {
+        $status = TestCase::getRandomHttpStatus();
+
+        $content = $apiResponses->statusCodeResponse($status['code'])->getContent();
+        $result = json_decode($content, true);
+
+        expect($result['message'])->toBe($status['message']);
+        expect($result['status']['code'])->toBe($status['code']);
+        expect($result['status']['message'])->toBe($status['message']);
+    });
+
+    it('can generate an Exception Response', function () use ($apiResponses) {
+
+        $exCode = mt_rand(0, 999);
+        $exMessage = TestCase::randomString();
+        $exception = new Exception($exMessage, $exCode);
+        $status = TestCase::getRandomHttpStatus();
+
+        $content = $apiResponses->exceptionResponse($exception, $status['code'])->getContent();
+        $result = json_decode($content, true);
+
+        expect($result['message'])->toBe($exMessage);
+        expect($result['status']['code'])->toBe($status['code']);
+        expect($result['status']['message'])->toBe($status['message']);
+
+
+        expect($result['data']['code'])->toBe($exCode);
+        expect($result['data']['file'])->toBeReadableFile();
+        expect($result['data']['line'])->toBeInt();
+    });
+
+    it('can generate a Not Implemented Response', function () use ($apiResponses) {
+
+
+        $content = $apiResponses->notImplementedResponse()->getContent();
+        $result = json_decode($content, true);
+
+
+        expect($result['message'])->toBe('Not Implemented');
+        expect($result['status']['code'])->toBe(501);
+        expect($result['status']['message'])->toBe('Not Implemented');
+
+
+        expect($result['meta']['timestamp'])->toBeString();
+    });
+});
